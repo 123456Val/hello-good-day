3ebb439221b8bfb87f5bae1c37b7dd2aac9935dc
123456Val <118950124+123456Val@users.noreply.github.com>
2023-03-11 15:51:21 -0600

    Add files via upload

---
+++ roblox.js
@@ -0,0 +1,12 @@
+roblox_arrive=[];
+function esh(){
+var wasaa=document.getElementById("robloxin").value;
+var wasa=document.getElementById("robloxon").value;
+var was=document.getElementById("robloxun").value;
+var wa=document.getElementById("roblox").value;
+roblox_arrive.push(wasaa);
+roblox_arrive.push(wasa);
+roblox_arrive.push(was);
+roblox_arrive.push(wa);
+document.getElementById("youtube").innerHTML=roblox_arrive;
+}
